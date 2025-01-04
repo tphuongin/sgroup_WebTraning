36a0ce16e76cf4abe764edf9e73d315d46121b7d
T Phuong <0511tphuong@gmail.com>
2025-01-04 12:31:03 +0700

    b7 b8

---
+++ B8/main.js
@@ -0,0 +1,12 @@
+const A = 'S-Group'
+console.log('xin chao')
+console.error("error")
+console.warn("warn")
+var b = 13
+let c =['helo', 133]
+console.log(b/c)
+console.log(b++)
+console.log(++b)
+
+
+
